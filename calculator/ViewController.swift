2a145282e3899d9bfe195bd4e328b317223971d6
longhoang4395 <longhoang4395@gmail.com>
2018-04-19 19:40:37 -0700

    Initial Commit

---
+++ calculator/ViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  calculator
+//
+
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var ketqua: UILabel!
+    
+    
+    var result = Float()
+    var a:Float = Float()
+    var c = 100
+    
+    var b = String()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        b = "="
+        ketqua.text = ("\(result)")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func btnNumber(_ sender: UIButton) {
+        a = a * 10 + Float(Int(sender.titleLabel!.text!)!)
+        ketqua.text = ("\(a)")
+        //result = a
+    }
+
+    @IBAction func btnHoatdong(_ sender: UIButton) {
+        switch b
+        {
+            case "=":
+            result = a
+        case "-":
+            result = result - a
+        case "+":
+            result = result + a
+        case "*":
+            result = result * a
+        case "/":
+            result = result / a
+        case "%":
+            result = result / 100.0
+        default:
+            print("Loi roi!!!")
+        }
+        a = 0
+        ketqua.text = ("\(result)")
+        if(sender.titleLabel!.text! == "=")
+        {
+            result = 0
+        }
+        b = sender.titleLabel!.text! as String!
+        
+    }
+    @IBAction func btnXoa(_ sender: UIButton) {
+        result = 0
+        a = 0
+        b = "="
+        ketqua.text = ("\(result)")
+    }
+}
+
